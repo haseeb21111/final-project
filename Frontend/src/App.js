601e4a5217d70cd7faf825b299289e781eff53fa
haseeb21111 <20055911@mydbs.ie>
2025-03-11 01:21:02 +0500

    called2

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,10 @@ import AppointmentList from './AppointmentList/AppointmentList';
 
 function App() {
   const [refreshList, setRefreshList] = useState(false);
+
+  const refreshAppointments = () => {
+    setRefreshList(!refreshList); // Triggers a re-fetch of data
+  };
   return (
     <div className="App">
       <h1>Appointment System</h1>
