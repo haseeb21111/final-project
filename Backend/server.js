93dda0e1811ddac75d3981125d84a02353c12ed8
haseeb21111 <20055911@mydbs.ie>
2025-04-05 11:26:03 +0500

    deleting appointment

--- Backend/server.js
+++ Backend/server.js
@@ -74,6 +74,7 @@ app.put('/api/appointments/:id', (req, res) => {
 
   app.delete('/api/appointments/:id', (req, res) => {
     const { id } = req.params;
+    const sql = 'DELETE FROM appointments WHERE id = ?';
   });
 
 });
