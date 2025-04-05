c38800617c6a7ca8ec00597b2b58bd3fa0893b6b
haseeb21111 <20055911@mydbs.ie>
2025-04-05 11:23:19 +0500

    handlerDelete complete

--- Frontend/src/AppointmentList/AppointmentList.jsx
+++ Frontend/src/AppointmentList/AppointmentList.jsx
@@ -32,15 +32,15 @@ const AppointmentList = () => {
       .catch((err) => console.error('Error updating!', err));
   };
   const handleDelete = (id) => {
-    if (window.confirm("Are you sure you want to delete this appointment?"))
-    axios.delete(`http://localhost:5000/api/appointments/${id}`)
-    .then(() => {
-      alert("Appointment deleted successfully!");
-      fetchAppointments();
-    })
-  .catch((err) => console.error("Error deleting appointment!", err));
-}
-  }
+    if (window.confirm("Are you sure you want to delete this appointment?")) {
+      axios.delete(`http://localhost:5000/api/appointments/${id}`)
+        .then(() => {
+          alert("Appointment deleted successfully!");
+          fetchAppointments();
+        })
+        .catch((err) => console.error("Error deleting appointment!", err));
+    }
+  };
  
   const handleEdit = (appointment) => {
     setEditingId(appointment.id);
